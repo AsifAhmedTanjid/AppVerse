0f2095eaab9c0c343c8468bb20b1fafe4b830e49
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-10-10 09:36:43 +0600

    modified the custom data

--- src/Components/Apps/AppCard.jsx
+++ src/Components/Apps/AppCard.jsx
@@ -9,8 +9,8 @@ const AppCard = ({ app }) => {
       <div className="card bg-base-100 w-96 shadow-sm">
         <figure>
           <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes" className="p-4 pb-0"
+            src={app.image}
+            alt="logo" className=" h-80 p-4 pb-0"
           />
         </figure>
         <div className="card-body">
